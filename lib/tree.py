e90ac6b54370f651718e7107705b5c359b80ed10
Jae Jeong <jeongjj29@gmail.com>
2024-09-03 23:50:10 -0400

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,34 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    """
+    {
+    'tag_name': 'h1',
+    'id': 'heading',
+    'text_content': 'Title',
+    'children': []
+    }
+    """
+
+    # Depth-First Approach
+    # def get_element_by_id(self, id):
+    #     nodes_to_visit = [self.root]
+    #     while len(nodes_to_visit) > 0:
+    #         node = nodes_to_visit.pop(0)
+    #         if node["id"] == id:
+    #             return node
+    #         else:
+    #             nodes_to_visit = node["children"] + nodes_to_visit
+    #     return None
+
+    # Breadth First Approach
+    def get_element_by_id(self, id):
+        nodes_to_visit = [self.root]
+        while len(nodes_to_visit) > 0:
+            node = nodes_to_visit.pop(0)
+            if node["id"] == id:
+                return node
+            else:
+                nodes_to_visit += node["children"]
+        return None
